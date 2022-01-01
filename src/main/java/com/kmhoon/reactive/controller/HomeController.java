f0fc9a2bfa9b7c401808a63c87d6f81882ae5dc2
K-MHoon <azmria117@gmail.com>
2022-01-01 23:40:54 +0900

    feat : 장바구니 화면 구현
    - item 리스트 목록 추가
    - Cart 목록 추가

--- src/main/java/com/kmhoon/reactive/controller/HomeController.java
+++ src/main/java/com/kmhoon/reactive/controller/HomeController.java
@@ -1,14 +1,29 @@
 package com.kmhoon.reactive.controller;
 
+import com.kmhoon.reactive.domain.Cart;
+import com.kmhoon.reactive.repository.CartRepository;
+import com.kmhoon.reactive.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
+
     @GetMapping
-    Mono<String> home() {
-        return Mono.just("home");
+    Mono<Rendering> home() {
+        return Mono.just(Rendering.view("home.html")
+                .modelAttribute("items",
+                        this.itemRepository.findAll())
+                .modelAttribute("cart",
+                        this.cartRepository.findById("My Cart")
+                                .defaultIfEmpty(new Cart("My Cart")))
+                .build());
     }
 }
