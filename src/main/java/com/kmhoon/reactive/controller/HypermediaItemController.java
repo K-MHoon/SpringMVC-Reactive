bdae4b49baf10d1257135fb7767eb4fcdc3294fa
K-MHoon <azmria117@gmail.com>
2022-01-16 22:37:00 +0900

    feat : HyberMedia Controller 생성 및 테스트
    - BlackHound 임시 제거

---
+++ src/main/java/com/kmhoon/reactive/controller/HypermediaItemController.java
@@ -0,0 +1,43 @@
+package com.kmhoon.reactive.controller;
+
+import com.kmhoon.reactive.domain.Item;
+import com.kmhoon.reactive.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
+
+@RestController
+@RequiredArgsConstructor
+public class HypermediaItemController {
+
+    private final ItemRepository repository;
+
+    @GetMapping("/hypermedia/items/{id}")
+    Mono<EntityModel<Item>> findOne(@PathVariable String id) {
+        HypermediaItemController controller = methodOn(HypermediaItemController.class);
+
+        Mono<Link> selfLink = linkTo(controller.findOne(id)).withSelfRel().toMono();
+        Mono<Link> aggregateLink = linkTo(controller.findAll()).withRel(IanaLinkRelations.ITEM).toMono();
+
+        return Mono.zip(repository.findById(id), selfLink, aggregateLink)
+                .map(o -> EntityModel.of(o.getT1(), Links.of(o.getT2(), o.getT3())));
+    }
+
+    @GetMapping("/hypermedia/items")
+    Mono<CollectionModel<EntityModel<Item>>> findAll() {
+
+        return this.repository.findAll() //
+                .flatMap(item -> findOne(item.getId())) //
+                .collectList() //
+                .flatMap(entityModels -> linkTo(methodOn(HypermediaItemController.class) //
+                        .findAll()).withSelfRel() //
+                        .toMono() //
+                        .map(selfLink -> CollectionModel.of(entityModels, selfLink)));
+    }
+}
