9df96492d3d320849878edae34b2137c8f65ba95
K-MHoon <azmria117@gmail.com>
2022-01-20 22:37:09 +0900

    feat : RSocketService 구현2
    - 요청 스트림 R소켓 익스체인지 처리
    - 실행 후 망각 R소켓 익스체인지 처리
    - R소켓 익스체인지 채널 모니터링 처리

--- src/main/java/com/kmhoon/reactive/service/RSocketService.java
+++ src/main/java/com/kmhoon/reactive/service/RSocketService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
@@ -25,5 +26,27 @@ public class RSocketService {
     public Mono<Item> processNewItemsViaRSocketRequestResponse(Item item) {
         return this.repository.save(item)
                 .doOnNext(savedItem -> this.itemSink.next(savedItem));
+    }
+
+    @MessageMapping("newItems.request-stream")
+    public Flux<Item> findItemsViaRSocketRequestStream() {
+        return this.repository.findAll()
+                .doOnNext(this.itemSink::next);
+    }
+
+    @MessageMapping("newItems.fire-and-forget")
+    public Mono<Void> processNewItemsViaRSocketFilterAndForget(Item item) {
+        return this.repository.save(item)
+                .doOnNext(savedItem -> this.itemSink.next(savedItem))
+                // Deprecated인 FluxProcessor, EmitterProcessor의 대체 구현
+                // .doOnNext(savedItem -> this.itemsSink.tryEmitNext(savedItem))
+                .then();
+    }
+
+    @MessageMapping("newItems.monitor")
+    public Flux<Item> monitorNewItems() {
+        return this.itemProcessor;
+        // Deprecated인 FluxProcessor, EmitterProcessor의 대체 구현
+        // return this.itemsSink.asFlux();
     }
 }
